57b1190fcc254ed22e8bdd3b58010664e625fbbe
rc1 <rc1@localhost>
2010-01-22 17:51:15 +0000

    renamed README.mk

---
+++ README.mk
@@ -0,0 +1,3 @@
+Modified stylesheet for the Doxygen Documentation. 
+
+Usage: Replace doxygen.css & tabs.css, add img_downArrow.png & doxygen.png to your docmentation directory. Please feel free to modify.
