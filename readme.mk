f28403ddca8cbbe651c9549c41ab08735e848754
rc1 <rc1@localhost>
2010-01-22 18:14:39 +0000

    removed README using wiki

--- readme.mk
+++
@@ -1,3 +0,0 @@
-Modified stylesheet for the Doxygen Documentation. 
-
-Usage: Replace doxygen.css & tabs.css, add img_downArrow.png & doxygen.png to your docmentation directory. Please feel free to modify.
